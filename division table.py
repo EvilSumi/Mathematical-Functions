1536c3802e23b61613fef474e353322e4871f062
EvilSumi <109980985+EvilSumi@users.noreply.github.com>
2022-08-21 22:08:19 +0530

    Add files via upload

---
+++ division table.py
@@ -0,0 +1,8 @@
+num = 23
+
+for i in range(1,24):
+    print( num, "divide by", i, '=' , num/i)
+
+
+
+
---
+++ signum fun.py
@@ -0,0 +1,12 @@
+i=24
+def signum():
+    if i<0:
+        print("Signum{", i,"} is -1")
+    elif i==0:
+        print("Signum{", i,"} is 0")
+    else:
+        print("Signum{", i,"} is 1")
+
+
+
+signum()
